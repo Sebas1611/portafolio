153378929b09f89f1d6e8ae6c53dcb96631b024c
Sebastián <104029805+Sebas1611@users.noreply.github.com>
2023-11-02 21:15:41 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+const typed = new Typed ('.typed', {
+    strings:[
+        'Developer',
+        'Front-End',
+        'Backend',
+        'Freelancer'
+    ],
+    typeSpeed:75,
+    startDelay: 200,
+    backSpeed: 75,
+    smartBackspace: true,
+    shuflle: false,
+    backDelay: 1000,
+    loop:true,
+    loopCount:false,
+    showCursor:true,
+    cursorChar: '|',
+    contentType: 'html'
+
+
+});
+
